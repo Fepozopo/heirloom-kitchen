57145ff3e462238f677c1acbefbef641e83e2b8c
Shane DuPrey <36708583+Fepozopo@users.noreply.github.com>
2025-04-05 19:55:58 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Generate the site
+# Build and run the project
 echo "Generating the site..."
 go build -o heirloom-kitchen
 ./heirloom-kitchen
-
-# Check if the build was successful
-if [ $? -ne 0 ]; then
-    echo "Build failed. Exiting."
-    exit 1
-fi
-
-echo "Site generated successfully."
